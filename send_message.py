85b4f25f1f2825f97f6586ac675b69830c44c6ba
jieran233 <jieranjiang233@gmail.com>
2024-01-20 02:54:19 +0800

    Add '--message_file' argument for 'send_message.py'

--- send_message.py
+++ send_message.py
@@ -4,7 +4,11 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--message', required=True, help='Message (required)')
+
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--message', help='Specify the message')
+    group.add_argument('--message_file', help='Specify the file containing the message')
+
     parser.add_argument('--session_token', required=True, help='Session token (required)')
     parser.add_argument('--conversation_id', required=True, help='Conversation ID (required)')
 
@@ -16,6 +20,11 @@ def main():
     parser.add_argument('--input_delay', type=float, default=0.1, help='Input delay')
 
     args = parser.parse_args()
+    if args.message:
+        input_message = args.message
+    elif args.message_file:
+        with open(args.message_file, 'r', encoding='utf-8') as f:
+            input_message = f.read()
 
     bot = Bot(
         session_token=args.session_token,
@@ -27,7 +36,7 @@ def main():
     )
 
     bot.new_chat()
-    output_message = bot.send_message(args.message, input_mode=args.input_mode, input_delay=args.input_delay)
+    output_message = bot.send_message(input_message, input_mode=args.input_mode, input_delay=args.input_delay)
 
     if output_message.failed:
         raise Exception("UnlimitedGPT Failed")
